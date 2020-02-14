3c7c566b32abbea15680f744fa479fd447815e29
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-02-14 11:36:56 -0500

    added screens and navigation

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import MealsNavigator from './assets/navigation/MealsNavigator';
+
+//const fetchFonts = ()=>{
+  //return Font.loadAsync({
+    //'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+   // 'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+ // });
+//};
+
 
 export default function App() {
+ // const [fondLoaded, setFontLoaded] = useState(false);
+  //if (!fondLoaded){
+    //return ( <AppLoading 
+    //startAsync= {fetchFonts}
+   // onFinish= {()=> setFontLoaded}
+    ///>
+    //);
+  //}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator/>
   );
 }
 
---
+++ assets/navigation/MealsNavigator.js
@@ -0,0 +1,17 @@
+import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer} from 'react-navigation';
+import CategoriesScreen from '../../screens/CategoriesScreen';
+import CategoryMealScreen from '../../screens/CategoryMealScreen';
+import MealDetailScreen from '../../screens/MealDetailScreen';
+
+
+
+const MealsNavigator = createStackNavigator({
+    Categories: CategoriesScreen,
+    CategoriesMeals: {
+        screen: CategoryMealScreen
+    },
+    MealDetal: MealDetailScreen
+});
+
+export default createAppContainer(MealsNavigator);
